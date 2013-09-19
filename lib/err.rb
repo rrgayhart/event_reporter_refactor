49e65251f3daa5408168378255b49fdd40d1691f
Meeka <rrgayhart@gmail.com>
2013-09-19 17:03:45 -0600

    creates basic print function

--- lib/err.rb
+++ lib/err.rb
@@ -1,9 +1,12 @@
 require 'csv'
 require 'pry'
-require './lib/file'
 
 class EventReporter
-include EventFile
+  
+  def test
+    input = gets.chomp
+    process_input(input)
+  end
 
   def run(input)
     @input = input.chomp
@@ -21,7 +24,8 @@ include EventFile
   end
 
   def default_file
-    "event_attendees.csv"
+    default = "event_attendees.csv"
+    return default.chomp
   end
   
   def one_command(command)
@@ -36,9 +40,9 @@ include EventFile
       assertion = assertion_array.join(" ")
       if command == "load" 
         if assertion.include? ".csv"
-          return "detected"
+          load_file(assertion)
         else
-          return load_file(default_file) 
+          load_file(default_file) 
         end
       else return "unrecognized"
       end
@@ -48,9 +52,24 @@ include EventFile
   end
 #-----------------------------------------------
 #file section and etc
-  def load_file(name)
-    file_name = name
-    return "#{file_name}"
+  def load_file(file)
+    file_name = file.chomp
+    @contents = CSV.read"#{file}", headers: true, header_converters: :symbol
+    print_file
   end
 
+  def clean_file(file)
+  end
+
+  def print_file
+    contents = @contents
+    print "ID\tLAST NAME\tFIRST NAME\tEMAIL\tZIPCODE\tCITY\tSTATE\tADDRESS\tPHONE\n"
+    contents.each do |row|
+      print "#{row[:id]}\t#{row[:last_name]}\t#{row[:first_name]}\t#{row[:email_address]}\t#{row[:zipcode]}\t#{row[:city]}\t#{row[:state]}\t#{row[:street]}\t#{row[:homephone]}\n"
+    end
+  end
 end
+
+
+e = EventReporter.new
+e.load_file('event_attendees.csv')

1e084d2dbfb5b8924d33268826b4095cfc3796af
Meeka <rrgayhart@gmail.com>
2013-09-19 20:02:02 -0600

    creates find_by function

--- lib/err.rb
+++ lib/err.rb
@@ -50,6 +50,7 @@ class EventReporter
 
   def three_command
   end
+
 #-----------------------------------------------
 #file section and etc
   def load_file(file)
@@ -67,14 +68,27 @@ class EventReporter
       end
       row[:zipcode].to_s.rjust(5,"0")[0..4]
     end
+  end
+
+  def find_by(attribute, query)
+    attribute = attribute.to_sym
+    searchable = @clean
+    @queue = []
+    searchable.each do |row|
+      if query.upcase == row[attribute].upcase
+        @queue << row
+    end
+    end
     print_file
   end
 
   def print_file
-    clean = @clean
-    print "ID\tLAST NAME\tFIRST NAME\tEMAIL\tZIPCODE\tCITY\tSTATE\tADDRESS\tPHONE\n"
+    clean = @queue
+    print "\t\tLAST NAME\tFIRST NAME\tEMAIL\tZIPCODE\tCITY\tSTATE\tADDRESS\tPHONE\n"
+    num = 0
     clean.each do |row|
-      print "#{row[:id]}\t#{row[:last_name]}\t#{row[:first_name]}\t#{row[:email_address]}\t#{row[:zipcode]}\t#{row[:city]}\t#{row[:state]}\t#{row[:street]}\t#{row[:homephone]}\n"
+      num += 1
+      print "#{num}\t#{row[:last_name]}\t#{row[:first_name]}\t#{row[:email_address]}\t#{row[:zipcode]}\t#{row[:city]}\t#{row[:state]}\t#{row[:street]}\t#{row[:homephone]}\n"
     end
   end
 end
@@ -82,3 +96,4 @@ end
 
 e = EventReporter.new
 e.load_file('event_attendees.csv')
+e.find_by('first_name', 'alice')
